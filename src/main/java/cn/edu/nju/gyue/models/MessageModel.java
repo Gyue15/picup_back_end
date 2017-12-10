b46166fbd462fe4b6401d79e307f50c793791e38
gyue15 <786330297@qq.com>
2017-12-10 20:36:20 +0800

    注释

--- src/main/java/cn/edu/nju/gyue/models/MessageModel.java
+++ src/main/java/cn/edu/nju/gyue/models/MessageModel.java
@@ -8,20 +8,44 @@ public class MessageModel {
 
     public Integer mid;
 
+    /**
+     * 消息内容
+     */
     public String text;
 
+    /**
+     * 消息拥有者
+     */
     public Integer owner;
 
+    /**
+     * true代表已读
+     */
     public Boolean isRead;
 
+    /**
+     * 消息制造者id
+     */
     public Integer uid;
 
+    /**
+     * 消息制造者名字
+     */
     public String username;
 
+    /**
+     * 消息制造者头像
+     */
     public String avatar;
 
+    /**
+     * 消息发送日期
+     */
     public Date date;
 
+    /**
+     * 格式化的消息发送日期
+     */
     public String formatDate;
 
     @Override
