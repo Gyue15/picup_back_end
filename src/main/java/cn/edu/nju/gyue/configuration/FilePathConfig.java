1d983473d61180333c1f98519d91e5c4a68e798a
151250148 <151250148@smail.nju.edu.cn>
2017-12-10 23:39:27 +0800

    ignore

--- src/main/java/cn/edu/nju/gyue/configuration/FilePathConfig.java
+++ src/main/java/cn/edu/nju/gyue/configuration/FilePathConfig.java
@@ -6,8 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class FilePathConfig extends WebMvcConfigurerAdapter {
-    public static final String PATH = "/Users/gyue/Pictures/MyPicture/";
-    public static final String AVATAR_PATH = "/Users/gyue/Pictures/MyPicture/avatar/";
+    public static final String PATH = "/Users/john/Pictures/picup/";
+    public static final String AVATAR_PATH = "/Users/john/Pictures/picup/avatar/";
+    public static final String AVATAR_NAME = "default.jpg";
 
     //供客户端使用的url前缀
     public static final String URL = "http://localhost:8080/picture/";
